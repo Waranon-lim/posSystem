5ea6540a31cb20757c22f76d0853db3eff49962d
Waranon Chokbunsuwan <lim@Maclim.local>
2026-03-22 17:48:06 +0800

    change new code

--- src/models/sale_item.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct SaleItem {
-    int    id          = 0;
-    int    sale_id     = 0;
-    int    product_id  = 0;
-    int    quantity    = 0;
-    double unit_price  = 0.0;   // snapshot of price at time of sale
-};
