6486a591d70882ae43e4f2436da92e7c60d672ac
Waranon Chokbunsuwan <lim@Maclim.local>
2026-03-24 22:06:17 +0800

    feat: add showMenu and getChoice

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,24 @@
+#include "services/menu.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int num;
-    cout << "1. Register \n";
-    cout << "2. Login\n";
-    cout << "3. Exit " << endl;
-    cin >> num;
+    showMenu();
 
-    if(num==1)
-    {
-        cout << "call register function" << endl;
-    }
-    else if(num==2)
-    {
-        cout << "call login function" << endl;
-    }
-    else
-    {
-        cout << "your exit the program";
-        return 0;
-    }
+    int choice = getChoice();
+    switch (choice)
+        {
+        case 1:
+            cout << "call register function";
+            break;
+        case 2:
+            cout << "call login function";
+            break;
+        case 3:
+            cout << "---your loging out---";
+            return 0;
+        }
     return 0;
 }
---
+++ src/services/menu.cpp
@@ -0,0 +1,27 @@
+#include "menu.h"
+#include <iostream>
+
+void showMenu()
+{
+     std::cout << "1. Register \n";
+      std::cout << "2. Login\n";
+      std::cout << "3. Exit " <<  std::endl;
+      std::cout << "choose your number : ";
+}
+
+int getChoice()
+{
+    while (true)
+    {
+
+        int num;
+         std::cin >> num;
+        if(num >= 1 && num <=3)
+        return num;
+        else 
+        {
+             std::cout << "number is invalid please try again" <<  std::endl;
+             std::cout << "choose your number again : " << std::endl;
+        }
+    }
+}
